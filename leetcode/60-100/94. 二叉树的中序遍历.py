8dc2d62102b69efcebee455cff59aa4ecca01b50
zzf-damon <zefeng_z@126.com>
2020-09-15 19:56:03 +0800

    zzf

--- leetcode/60-100/94. 二叉树的中序遍历.py
+++ leetcode/60-100/94. 二叉树的中序遍历.py
@@ -6,11 +6,7 @@ class TreeNode:
         self.right = None
 
 
-
-
-
-a = [1, None, 2, 3, None]
-
+a = [1, 2, 3, 4, 5, 6, 7, None, 8, 9, None, 10, 11, None, 12]
 
 root = TreeNode(a.pop(0))
 queue = [root]
@@ -27,11 +23,46 @@ while a:
 
 class Solution:
     def inorderTraversal(self, root):
-        if not root:return []
-        res 
-        def dfs():
+        if not root: return []
+        res = []
+        stack = []
+
+        def dfs(root):
+            stack.append(root.val)
+            if root.left:
+                dfs(root.left)
+            res.append(stack.pop())
+            if root.right:
+                dfs(root.right)
+
+        if root:
+            dfs(root)
+        return res
 
 
-        dfs()
-        return 
+s = Solution()
+print(s.inorderTraversal(root))
 
+
+def XianXu(root):
+    if root == None:
+        return
+    print(root.val)
+    XianXu(root.left)
+    XianXu(root.reft)
+
+
+def ZhongXu(root):
+    if root == None:
+        return
+    ZhongXu(root.left)
+    print(root.val)
+    ZhongXu(root.reft)
+
+
+def HouXu(root):
+    if root == None:
+        return
+    HouXu(root.left)
+    HouXu(root.reft)
+    print(root.val)
