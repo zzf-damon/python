7845d6fcb802e8a44a0d0497645b3090d622ee66
zzf-damon <zefeng_z@126.com>
2020-10-03 11:34:36 +0800

    10-3

--- leetcode/771. 宝石与石头.py
+++ leetcode/771. 宝石与石头.py
@@ -1,6 +1,10 @@
 from collections import Counter
 
+import requests
 
+url = "https://www.baidu.com/?tn=02003390_hao_pg"
+response = requests.get(url)
+print(response.text)
 class Solution:
     def numJewelsInStones(self, J: str, S: str) -> int:
         tem_list = Counter(S)
--- leetcode/前30/2两数之和.py
+++ leetcode/前30/2两数之和.py
@@ -10,10 +10,29 @@ class Solution:
             pre_list = nums[k + 1:]
             if j in pre_list:
                 j_index = pre_list.index(j)
-                print(i, k, j, j_index + k + 1)
                 return [k, j_index + k + 1]
 
 
 target = -8
 solution = Solution()
 print(solution.twoSum(nums, target))
+
+
+
+from typing import List
+
+class Solution1:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pre = 0 
+        while nums:
+            tem = nums.pop(0)
+            if (target- tem) in nums:
+                pro = nums.index(target-tem)
+                break
+            pre += 1
+        return [pre,pro+pre+1]
+
+
+s1 = Solution1()
+
+print(s1.twoSum(nums,target))
