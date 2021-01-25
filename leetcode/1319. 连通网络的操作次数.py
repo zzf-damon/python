70c479aca52813e730cd43d41f1a8c54240b754c
zzf-damon <zefeng_z@126.com>
2021-01-25 14:16:02 +0800

    累+=1

--- leetcode/1319. 连通网络的操作次数.py
+++ leetcode/1319. 连通网络的操作次数.py
@@ -5,37 +5,68 @@ from typing import List
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
         if len(connections) < n - 1: return -1
+        parent = list(range(n))
 
-        edge = collections.defaultdict(list)
-        rec = collections.defaultdict(list)
+        def find(index: int) -> int:
+            if parent[index] != index:
+                parent[index] = find(parent[index])
+            return parent[index]
 
-        for i, (x, y) in enumerate(connections):
-            rec[x].append(i)
-            rec[y + 100001].append(i)
+        def union(index1: int, index2: int):
+            parent[find(index1)] = find(index2)
 
-        for vec in rec.values():
-            k = len(vec)
-            for i in range(1, k):
-                edge[vec[i - 1]].append(vec[i])
-                edge[vec[i]].append(vec[i - 1])
-
-        def dfs(x: int):
-            vis.add(x)
-            for y in edge[x]:
-                if y not in vis:
-                    dfs(y)
-
-        vis = set()
-        num = 0
-        for i in range(n):
-            if i not in vis:
-                num += 1
-                dfs(i)
-
-        return n - num
+        for x, y in connections:
+            if find(x) != find(y):
+                union(x, y)
+                n -= 1
+        print(parent)
+        return n - 1
 
 
-n = 4
-connections = [[0, 1], [0, 2], [1, 2]]
+n = 12
+connections = [[0, 6], [0, 1], [0, 4], [0, 3], [2, 6], [0, 2], [1, 5], [1, 4], [1, 2], [1, 7], [3, 7], [4, 7], [3, 5]]
 s = Solution()
-s.makeConnected(n, connections)
+print(s.makeConnected(n, connections))
+
+
+# 并查集模板
+class UnionFind:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.size = [1] * n
+        self.n = n
+        # 当前连通分量数目
+        self.setCount = n
+
+    def findset(self, x: int) -> int:
+        if self.parent[x] == x:
+            return x
+        self.parent[x] = self.findset(self.parent[x])
+        return self.parent[x]
+
+    def unite(self, x: int, y: int) -> bool:
+        x, y = self.findset(x), self.findset(y)
+        if x == y:
+            return False
+        if self.size[x] < self.size[y]:
+            x, y = y, x
+        self.parent[y] = x
+        self.size[x] += self.size[y]
+        self.setCount -= 1
+        return True
+
+    def connected(self, x: int, y: int) -> bool:
+        x, y = self.findset(x), self.findset(y)
+        return x == y
+
+
+class Solution1:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        if len(connections) < n - 1:
+            return -1
+
+        uf = UnionFind(n)
+        for x, y in connections:
+            uf.unite(x, y)
+
+        return uf.setCount - 1
